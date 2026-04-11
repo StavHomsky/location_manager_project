923122d44ffd8885a18d17d351d01476f9607198
Inga Kaydanova <inga.kaidanova@jobleads.com>
2026-04-11 13:49:27 +0100

    Enhance delete all locations confirmation to be case-insensitive and provide feedback if not deleted

--- main.py
+++ main.py
@@ -134,8 +134,10 @@ while True:
 
     elif choice == "9":
         confirm = input("Are you sure you want to delete all locations? (y/n)")
-        if confirm == "y":
+        if confirm.lower() == "y":
             utils.delete_all_locations(locations)
+        else:
+            print("\nLocation not deleted")
         input("\nPress Enter to continue...")
 
     elif choice == "10":
